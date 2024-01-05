555228ca7682289f867db7ec4efc6c5fed647e29
THE-SIMPLE-MARK <57036815+THE-SIMPLE-MARK@users.noreply.github.com>
2024-01-05 19:54:48 +0100

    Move to TSup

---
+++ tsup.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "tsup"
+
+export default defineConfig(opts => ({
+	entry: ["src/index.ts"],
+	splitting: true,
+	clean: true,
+	dts: {
+		footer: 'declare module "bambu-node"',
+	},
+	format: "esm",
+	target: "es2020",
+	minify: true,
+	outDir: "dist",
+}))
