79db7f4d348a251053140a704c933068138d50e5
THE-SIMPLE-MARK <57036815+THE-SIMPLE-MARK@users.noreply.github.com>
2024-01-06 21:39:39 +0100

    Rework UpdateFanCommand implementation with enums

--- src/commands/UpdateFanCommand.ts
+++ src/commands/UpdateFanCommand.ts
@@ -2,11 +2,10 @@ import { GCodeCommand, GCodeCommandParam } from "./GCodeCommand"
 import type { NumberRange, ValueOf } from "src/types"
 import { isUpdateFanCommand } from "src/responses"
 
-interface Fans {
-	big_1: NumberRange<0, 100>
-	big_2: NumberRange<0, 100>
-	cooling: NumberRange<0, 100>
-	heatbreak: NumberRange<0, 100>
+export enum Fan {
+	PART_COOLING_FAN = 1,
+	AUXILIARY_FAN = 2,
+	CHAMBER_FAN = 3,
 }
 
 export class UpdateFanCommand extends GCodeCommand {
@@ -14,26 +13,15 @@ export class UpdateFanCommand extends GCodeCommand {
 
 	/**
 	 * Constructs a `print.gcode_line` command, which is able to update the current speeds of every fan on the printer.
-	 * @param fan {Fans} The fan's speed to update.
-	 * @param percent {ValueOf<Fans>} The fan's new speed in % (0-100)
+	 * @param fan {Fan} The fan's speed to update.
+	 * @param percent {NumberRange<0, 100>} The fan's new speed in % (0-100)
 	 */
-	public constructor(fan: keyof Fans, percent: ValueOf<Fans>) {
-		let fanId = ""
-		switch (fan) {
-			case "cooling": // part cooling fan
-				fanId = "P1"
-				break
-			case "big_1": // aux fan
-				fanId = "P2"
-				break
-			case "big_2":
-				fanId = "P3"
-				break
-			default:
-				throw new Error("Unknown fan!")
-		}
+	public constructor(fan: Fan, percent: NumberRange<0, 100>) {
+		// check if the given number is not in the enum
+		if (!Object.values(Fan).includes(fan))
+			throw new Error("An unknown fan was specified!")
 
-		super([`M106 ${fanId} S${(255 * percent) / 100}`])
+		super([`M106 P${fan.toString()} S${(255 * percent) / 100}`])
 	}
 
 	ownsResponse = isUpdateFanCommand
