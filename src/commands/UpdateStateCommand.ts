6a3bdb04c685686bfb4bd91af8615767f929ff35
THE-SIMPLE-MARK <57036815+THE-SIMPLE-MARK@users.noreply.github.com>
2024-01-06 20:15:43 +0100

    Remove leftover hardcoded sequence_id implementation

--- src/commands/UpdateStateCommand.ts
+++ src/commands/UpdateStateCommand.ts
@@ -2,7 +2,6 @@ import { AbstractCommand } from "./AbstractCommand"
 import type { CommandInterface } from "./CommandInterface"
 import { isUpdateStateCommand } from "src/responses"
 
-const sequenceMap = { pause: 2008, resume: 2009, stop: 2010 }
 export type State = "pause" | "resume" | "stop"
 
 export class UpdateStateCommand extends AbstractCommand {
@@ -16,7 +15,6 @@ export class UpdateStateCommand extends AbstractCommand {
 	public constructor(state: State) {
 		super()
 		this.command = state
-		this.sequenceId = sequenceMap[state]
 	}
 
 	ownsResponse = isUpdateStateCommand
